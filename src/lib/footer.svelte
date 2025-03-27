fbc7d017de3e4b72f0f55ab14357eb7729ab5384
Mahad Kalam <mahadkalam1@proton.me>
2025-03-27 21:36:48 +0000

    Fix timeAgo function

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -12,12 +12,12 @@
     const now = new Date();
     const diff = Math.floor((now.getTime() - timestamp) / 1000); // Difference in seconds
 
-    if (diff < 60) return `${diff} seconds ago`;
+    if (diff < 60) return `${diff} second${diff !== 1 ? "s" : ""} ago`;
     if (diff < 3600)
-      return `${Math.floor(diff / 60)} minute${Math.floor(diff / 60) > 1 ? "s" : ""} ago`;
+      return `${Math.floor(diff / 60)} minute${Math.floor(diff / 60) !== 1 ? "s" : ""} ago`;
     if (diff < 86400)
-      return `${Math.floor(diff / 3600)} hour${Math.floor(diff / 3600) > 1 ? "s" : ""} ago`;
-    return `${Math.floor(diff / 86400)} day${Math.floor(diff / 86400) > 1 ? "s" : ""} ago`;
+      return `${Math.floor(diff / 3600)} hour${Math.floor(diff / 3600) !== 1 ? "s" : ""} ago`;
+    return `${Math.floor(diff / 86400)} day${Math.floor(diff / 86400) !== 1 ? "s" : ""} ago`;
   }
 
   const commitAge = timeAgo(commitTime);
